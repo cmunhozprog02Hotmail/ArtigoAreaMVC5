29e172b1144f633f0e5c3f34eda510f982bd58cd
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-10-03 14:31:54 -0300

    Primeiro Commit

---
+++ ArtigoAreaMVC5/ArtigoAreaMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtigoAreaMVC5.Startup))]
+namespace ArtigoAreaMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
